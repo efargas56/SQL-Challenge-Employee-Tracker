3a742fec3c8dff521e43242c19c8d0cc61bda0e1
ef56 <efargas56@gmail.com>
2022-07-26 01:41:55 -0400

    added schema file

---
+++ db/schema.sql
@@ -0,0 +1,20 @@
+DROP DATABASE IF EXISTS employeesDB
+
+CREATE DATABASE employeesDB;
+
+USE employeesDB;
+
+CREATE TABLE department (
+    id INTEGER AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(30) NULL,
+    salary DECIMAL,
+    department_id INTEGER
+)
+
+CREATE TABLE employee (
+   id INTEGER AUTO_INCREMENT PRIMARY KEY,
+   first_name VARCHAR(30) NULL,
+   last_name VARCHAR(30) NULL,
+   role_id INTEGER,
+   manager_id INTEGER
+   )
